d88a6e25096e2313853f0efc66933ceec815312f
Chiara Seim <chiara@althea.systems>
2025-06-04 21:23:11 -0700

    Add throughput and clients online to dashboard

--- src/Frontpage/ExitStatus/index.js
+++ src/Frontpage/ExitStatus/index.js
@@ -1,23 +1,25 @@
 import React, { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
-import { Card, Heading } from "ui";
+import { Card } from "ui";
 import { get } from "store";
 import greencheck from "../../images/greencheck.svg";
 import redx from "../../images/redx.svg";
 
 const Indicator = ({ condition }) => (
-    <img
-      src={condition ? greencheck : redx}
-      alt={condition ? "checkmark" : "x symbol"}
-      className="mr-1"
-    />
-  );
+  <img
+    src={condition ? greencheck : redx}
+    alt={condition ? "checkmark" : "x symbol"}
+    className="mr-1"
+  />
+);
 
 // This page monitors the startup status of the exit TODO
 // expand to monitor the online status rather than just the startup status
 const StartupStatus = () => {
   const [t] = useTranslation();
   const [exitStartupStatus, setExitStartupStatus] = useState(false);
+  const [exitThroughput, setExitThroughput] = useState(0);
+  const [exitClients, setExitClients] = useState(0);
 
   useEffect(() => {
     const controller = new AbortController();
@@ -28,6 +30,15 @@ const StartupStatus = () => {
         if (!(exitStartupStatus instanceof Error))
           setExitStartupStatus(exitStartupStatus);
       } catch (e) {}
+      try {
+        let exitThroughput = await get("/throughput");
+        if (!(exitThroughput instanceof Error))
+          setExitThroughput(exitThroughput);
+      } catch (e) {}
+      try {
+        let exitClients = await get("/clients");
+        if (!(exitClients instanceof Error)) setExitClients(exitClients);
+      } catch (e) {}
     })();
 
     return () => controller.abort();
@@ -35,20 +46,58 @@ const StartupStatus = () => {
 
   let startupStatusDisplay;
   if (exitStartupStatus) {
-    startupStatusDisplay = <>{Indicator(false)}{exitStartupStatus}</>
+    startupStatusDisplay = (
+      <>
+        {Indicator(false)}
+        {exitStartupStatus}
+      </>
+    );
   } else {
-    startupStatusDisplay = <>{Indicator(true)}{t("running")}</>;
+    startupStatusDisplay = (
+      <>
+        {Indicator(true)}
+        {t("running")}
+      </>
+    );
   }
+  let throughputDisplay = (
+    <>
+      {t("ExitThroughput")}: {exitThroughput} {t("bps")}
+    </>
+  );
+  let clientsDisplay = (
+    <>
+      {t("ExitClients")}: {exitClients}
+    </>
+  );
 
   return (
     <Card>
-      <Heading title={t("ExitStartup")} />
+      <div style={{ fontSize: "1.5rem", fontWeight: 500, marginBottom: 10 }}>
+        {t("ExitStartup")}
+      </div>
 
       <div
         className="d-flex flex-wrap flex-md-nowrap w-100"
-        style={{ marginTop: -10 }}
+        style={{ marginBottom: 5 }}
       >
         {startupStatusDisplay}
+      </div>
+      <div
+        className="d-flex flex-wrap flex-md-nowrap w-100"
+        style={{ marginTop: 15 }}
+      >
+        <div style={{ fontSize: "1.5rem", fontWeight: 500 }}>
+          {throughputDisplay}
+        </div>
+      </div>
+      <div
+        className="d-flex flex-wrap flex-md-nowrap w-100"
+        style={{ marginTop: 5 }}
+      >
+        <div style={{ fontSize: "1.5rem", fontWeight: 500 }}>
+          {clientsDisplay}
+        </div>
       </div>
     </Card>
   );
